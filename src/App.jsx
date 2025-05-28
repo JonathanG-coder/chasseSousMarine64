12066f20396d7dd7341a4db13a0402fb07b34562
Jonathan <jonathangager75@gmail.com>
2025-05-28 16:28:41 +0200

    Instalalation project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from "./pages/home/Home"
+import './App.css';
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+export default function Header(){
+
+    return (
+        <header>
+            <h1>bonjour</h1>
+        </header>
+
+    );
+}
