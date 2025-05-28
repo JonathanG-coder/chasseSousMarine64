f5ee02824738faf6fba090766c152769a02e2429
Jonathan <jonathangager75@gmail.com>
2025-05-28 16:11:01 +0200

    Create and installation component project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from "./pages/home/Home"
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
