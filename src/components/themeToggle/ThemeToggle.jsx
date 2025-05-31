5729b6492b69a67b817829fcacd2f12aaffc3172
Jonathan <jonathangager75@gmail.com>
2025-05-31 15:25:17 +0200

    Add icon lightHouse to DarkMode and title project

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,11 +1,24 @@
 import { useTheme } from "../../context/themeContext";
+import "./ThemeToggle.css";
+
+//Import des images en clair et sombre
+import iconLightHouseB from "../../assets/iconDarkMode/lightHouseB.png";
+import iconLightHouseW from "../../assets/iconDarkMode/lightHouseW.png";
+
+const lightIconW = iconLightHouseW
+const lightIconB = iconLightHouseB
 
 export default function ThemeToggle() {
   const { darkMode, toggleDarkMode } = useTheme();
 
   return (
+    <div className="ThemeTOggleBody">
     <button onClick={toggleDarkMode}>
-      {darkMode ? "☀️ Mode Clair" : "🌙 Mode Sombre"}
+      <img src={darkMode ? lightIconB : lightIconW} alt="lightHouse" className="iconLightHouse"></img>
     </button>
+    <div className="titleBanniere">
+    <h1>Chasse sous marine 64</h1>
+    </div>
+    </div>
   );
 }
