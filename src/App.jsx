07883ce3ada916da24b4e96f7a9132587cd153a4
Jonathan <jonathangager75@gmail.com>
2025-06-03 16:24:31 +0200

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
-// import ThemeToggle from "./components/themeToggle/ThemeToggle";
+// import Footer from "./components/footer/Footer";
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
+      {/* <Footer /> */}
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,8 @@ export default function Footer() {
 
 
   return (
-    <></>
+    <>
+    <p>Create by Jonathan </p>
+    </>
      );
 }
