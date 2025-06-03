b673dabe83c4b270d265bcac8395dfda96df2dac
Jonathan <jonathangager75@gmail.com>
2025-06-03 20:49:44 +0200

    Add file spot for map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
+import Spot from  "./pages/spot/Spot"
 // import Footer from "./components/footer/Footer";
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ← importe le Router
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/themeContext";
+import 'leaflet/dist/leaflet.css'; // Permet d'apporter le style css pour la carte personnalisé.
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
