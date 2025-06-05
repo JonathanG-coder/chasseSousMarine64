90068f50c4d16a4b61ef0000bb86e8f789bf23bd
Jonathan <jonathangager75@gmail.com>
2025-06-05 12:07:42 +0200

    Create components folder and file for page Especes

--- src/pages/especes/Especes.jsx
+++ src/pages/especes/Especes.jsx
@@ -4,41 +4,14 @@
 // 🦞 Crustacés présents sur la côte
 // 🐟 Poissons côtiers fréquents
 
+import React from 'react'
 
-
-  const renderCategory = (title, items) => {
-    console.log("Catégorie :", title);
-    console.log("Données reçues :", items);
-
-    return (
-      <div>
-        <h2>{title}</h2>
-        <div>
-          {items.map((item, index) => (
-            <div key={index}>
-              <img src={item.image} alt={item.nom} />
-              <h4>{item.nom}</h4>
-              <p>{item.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
-
+const Especes = () => {
   return (
-    <div className="especes-container">
-      <h1>Faune et flore marine côtière du 64</h1>
-
-      {/* {renderCategory("Poissons côtiers fréquents", Poissons)}
-      {renderCategory("Crustacés présents sur la côte", Crustaces)}
-      {renderCategory("Céphalopodes observables en bord de côte", Cephalopodes)}
-      {renderCategory("Végétaux marins et algues", VégétauxMarinsAlgues)}
-      {renderCategory("Animaux fixés / invertébrés", InvertebresAnimauxFixes)}
-      {renderCategory("Coquillages", Coquillages)}
-      {renderCategory("Types de rochers côtiers", Rochers)} */}
+    <div>
+        <h1>Faune et flore marine côtière du 64 </h1>
     </div>
-  );
-};
+  )
+}
 
-export default Especes;
+export default Especes
